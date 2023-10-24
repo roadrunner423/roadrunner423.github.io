637cb40f429752165650615fcf129b431d44b684
roadrunner423 <148813320+roadrunner423@users.noreply.github.com>
2023-10-23 20:54:25 -0600

    New stuff after learning a bit

---
+++ script.js
@@ -0,0 +1,20 @@
+class hi{
+    constructor(pass){  
+        this.pass = pass;
+    }
+    checkPass(){
+        if(this.pass == "1234"){
+            return(true);
+        }else{
+            return(false);
+        }
+    }
+
+}
+let pass = prompt("Enter the password");
+let allow = new hi(pass);
+if(allow.checkPass() == false){
+    window.location.replace("index.html");
+}
+//text printing system
+//document.getElementById("text").innerHTML = "<p>" + allow.checkPass() + "</p>"
